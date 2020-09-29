f743b22ad2f90db5b59c6a0be75381bc62b4f6a2
Irina <ichubarkina@linguatrip.com>
2020-09-29 19:26:08 +0300

    fix try catch

--- Service/Program.cs
+++ Service/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.IO;
 using Service.Services;
 using Service.Utils;
 using Service.Validators;
@@ -10,82 +9,73 @@ namespace Service
     {
         static void Main(string[] args)
         {
-            ConsoleValidator.ValidateArgsAndThrow(args);
-            var sourceFilePath = ConsoleHelper.GetSourceFilePath(args);
-            var method = args[1];
+            try
+            {
+                ConsoleValidator.ValidateArgsAndThrow(args);
+                var sourceFilePath = ConsoleHelper.GetSourceFilePath(args);
+                var method = args[1];
 
-            if (args[0] == "encode")
-            {
-                var word = args[3];
-                var wordAsBits = string.Empty;
-                foreach (var character in word)
+                switch (args[0])
                 {
-                    wordAsBits += Constants.Constants.RussianCharactersToCodes[character];
+                    case "encode":
+                    {
+                        var word = args[3];
+                        var wordAsBits = string.Empty;
+                        foreach (var character in word)
+                        {
+                            wordAsBits += Constants.Constants.RussianCharactersToCodes[character];
+                        }
+                        var targetFilePath = ConsoleHelper.GetTargetFilePath(args);
+                        HandleEncodeMode(method, sourceFilePath, targetFilePath, wordAsBits, word);
+                        break;
+                    }
+                    case "decode":
+                        HandleDecodeMode(method, sourceFilePath);
+                        break;
+                    default:
+                        throw new ArgumentException("Wrong mode");
                 }
-                var targetFilePath = ConsoleHelper.GetTargetFilePath(args);
-                HandleEncodeMode(method, sourceFilePath, targetFilePath, wordAsBits, word);
             }
-            else if (args[0] == "decode")
+            catch (Exception e)
             {
-                HandleDecodeMode(method, sourceFilePath);
+                Console.WriteLine(e.Message);
             }
+            
         }
 
         private static void HandleDecodeMode(string method, string sourceFilePath)
         {
-            try
+            var word = method switch
             {
-                string word;
-                switch (method)
-                {
-                    case "1":
-                        word = SpecialSymbolsService.Decode(sourceFilePath);
-                        break;
-                    case "2":
-                        word = EndSpacesService.Decode(sourceFilePath);
-                        break;
-                    case "3":
-                        word = ReplacementService.Decode(sourceFilePath);
-                        break;
-                    default:
-                        throw new ArgumentException("Wrong method");
-                }
+                "1" => SpecialSymbolsService.Decode(sourceFilePath),
+                "2" => EndSpacesService.Decode(sourceFilePath),
+                "3" => ReplacementService.Decode(sourceFilePath),
+                _ => throw new ArgumentException("Wrong method")
+            };
 
-                Console.WriteLine("Word: " + word);
-                ConsoleHelper.ShowFileSize(sourceFilePath);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }   
+            Console.WriteLine("Word: " + word);
+            ConsoleHelper.ShowFileSize(sourceFilePath);
         }
 
         private static void HandleEncodeMode(string method, string sourceFilePath, string targetFilePath, string wordAsBits, string word)
         {
-            try
+            switch (method)
             {
-                switch (method)
-                {
-                    case "1":
-                        SpecialSymbolsService.Encode(sourceFilePath, targetFilePath, wordAsBits);
-                        break;
-                    case "2":
-                        EndSpacesService.Encode(targetFilePath, sourceFilePath, wordAsBits);
-                        break;
-                    case "3":
-                        ReplacementService.Encode(targetFilePath, sourceFilePath, wordAsBits, word);
-                        break;
-                    default:
-                        throw new ArgumentException("Wrong method");
-                }
-                Console.WriteLine("Success");
-                ConsoleHelper.ShowFileSize(sourceFilePath);
-                ConsoleHelper.ShowFileSize(targetFilePath);
+                case "1":
+                    SpecialSymbolsService.Encode(sourceFilePath, targetFilePath, wordAsBits);
+                    break;
+                case "2":
+                    EndSpacesService.Encode(targetFilePath, sourceFilePath, wordAsBits);
+                    break;
+                case "3":
+                    ReplacementService.Encode(targetFilePath, sourceFilePath, wordAsBits, word);
+                    break;
+                default:
+                    throw new ArgumentException("Wrong method");
             }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }   
+            Console.WriteLine("Success");
+            ConsoleHelper.ShowFileSize(sourceFilePath);
+            ConsoleHelper.ShowFileSize(targetFilePath);
         }
     }
 }
